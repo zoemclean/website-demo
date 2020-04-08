1b8f911bf1e6fde00bd74c1ddc5561935c341772
Zoe McLean <zoe@Zoes-MacBook-Pro.local>
2020-04-09 10:31:25 +1200

    Added a banner image and an alternatig section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $('#navListButton').click(function(){
     $('#navList').slideToggle(300);
 });
+
+
+
+
+let winH = $(window).height() * 0.8;
+
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > winH ) {
+        $('#nav').addClass('bgRed');
+        $('#headerImg').attr('src', 'images/colab-logo-alt.png');
+    } else {
+        $('#nav').removeClass('bgRed');
+        $('#headerImg').attr('src', 'images/colab-logo-white.png');
+    }
+}).on('resize', function(){
+   winH = $(this).height() * 0.8;
+});
